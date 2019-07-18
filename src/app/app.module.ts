6530a026c7d45df56c5b19933e24bf63af333bc2
Omar Paredes <omare.paredes@gmail.com>
2019-07-18 00:40:20 -0500

    Inicio Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { OfferComponent } from './components/offer/offer.component';
+import { AboutComponent } from './components/about/about.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MapComponent } from './components/map/map.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    SliderComponent,
+    OfferComponent,
+    AboutComponent,
+    TeachersComponent,
+    ContactComponent,
+    MapComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
